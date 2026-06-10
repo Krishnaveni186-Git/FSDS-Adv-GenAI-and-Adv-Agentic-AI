95d874fa1a47e6bf281eaf6b2f6b8a20317bc58f
Krishnaveni Gadiya <Krishnanatalikcg@gmail.com>
2026-06-10 18:20:06 +0530

    Update SQL Practice 61 - 82.sql

--- DB/SQL Practice 61 - 82.sql
+++ DB/SQL Practice 61 - 82.sql
@@ -84,15 +84,6 @@ WHERE SALARY LIKE '_2%3'	Finds any values that have a 2 in the second position a
 WHERE SALARY LIKE '2___3'	Finds any values in a five-digit number that start with 2 and end with 3
 
   
-     Statement	                        Description
-WHERE SALARY LIKE '200%'	Finds any values that start with 200
-WHERE SALARY LIKE '%200%'	Finds any values that have 200 in any position
-WHERE SALARY LIKE '_00%'	Finds any values that have 00 in the second and third positions
-WHERE SALARY LIKE '2_%_%'	Finds any values that start with 2 and are at least 3 characters in length
-WHERE SALARY LIKE '%2'	        Finds any values that end with 2
-WHERE SALARY LIKE '_2%3'	Finds any values that have a 2 in the second position and end with a 3
-WHERE SALARY LIKE '2___3'	Finds any values in a five-digit number that start with 2 and end with 3
-
 select * from customers
 where salary like '200%';
 
